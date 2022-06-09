b286ed046b9675b74766d186577aa7b19d99df06
Pratham Choudhary <prathamchoudhary23@gmail.com>
2022-06-09 17:53:26 +0530

    Added Day 5 programs

---
+++ Day_5/merge_two_sorted_linkedlist.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the linked list node structure.
+    
+    template <typename T>
+    class Node {
+        public:
+        T data;
+        Node* next;
+
+        Node(T data) {
+            next = NULL;
+            this->data = data;
+        }
+
+        ~Node() {
+            if (next != NULL) {
+                delete next;
+            }
+        }
+    };
+
+************************************************************/
+
+Node<int>* sortTwoLists(Node<int>* l1, Node<int>* l2)
+{
+    // Write your code here.
+    if(l1 == NULL) return l2;
+    if(l2 == NULL) return l1;
+    if(l1->data>l2->data) swap(l1,l2);
+    Node<int>*res = l1;
+    while(l1!=NULL && l2!=NULL)
+    {
+        Node<int>*temp = NULL;
+        while(l1!=NULL && (l1->data <= l2->data))
+        {
+            temp = l1;
+            l1 = l1->next;
+        }
+        temp->next = l2;
+        swap(l1,l2);
+    }
+    return res;
+}
