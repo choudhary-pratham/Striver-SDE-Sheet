2076aebd77601c726299a34b463b2020421785f1
Pratham Choudhary <prathamchoudhary23@gmail.com>
2022-06-11 13:35:58 +0530

    added day 7 programs

--- Day_7/remove_duplicates_from_sorted_array.cpp
+++ Day_7/remove_duplicates_from_sorted_array.cpp
@@ -1,12 +1,12 @@
 int removeDuplicates(vector<int> &arr, int n) {
 	// Write your code here.
+    if(arr.size() == 0) return 0;
     int i=0;
-    for(int j=1;j<n;j++)
+    for(int j=1;j<n;j++) 
     {
         if(arr[i] != arr[j])
         {
-            i++;
-            arr[i] = arr[j];
+            arr[++i] = arr[j];
         }
     }
     return (i+1);
