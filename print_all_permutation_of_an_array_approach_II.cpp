2cd8cfafc8778c08dc0b566bd15ba78f0314445b
Pratham Choudhary <prathamchoudhary23@gmail.com>
2022-06-14 21:29:31 +0530

    added day 10 programs

---
+++ print_all_permutation_of_an_array_approach_II.cpp
@@ -0,0 +1,20 @@
+void f(int ind,string s, vector<string>&ans)
+{
+    if(ind == s.size())
+    {
+        ans.push_back(s);
+        return;
+    }
+    for(int i=ind;i<s.size();i++)
+    {
+        swap(s[ind],s[i]);
+        f(ind+1,s,ans);
+        swap(s[ind],s[i]);
+    }
+}
+vector<string> findPermutations(string &s) {
+    // Write your code here.
+    vector<string>ans;
+    f(0,s,ans);
+    return ans;
+}
