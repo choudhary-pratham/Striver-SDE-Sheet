d944a5d50bb838702df12c4f5b897bb2e5a9122c
Pratham Choudhary <prathamchoudhary23@gmail.com>
2022-06-19 16:24:59 +0530

    added day 11 programs

---
+++ Day_11/chess_tournament.cpp
@@ -0,0 +1,37 @@
+int isPossible(vector<int>&arr,int n,int c,int dist)
+{
+    int co_ord = arr[0],players=1;
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i]-co_ord>=dist)
+        {
+           co_ord = arr[i];
+           players++; 
+        }
+        if(players == c) return true;
+    }
+    return false;
+}
+
+
+int chessTournament(vector<int> arr , int n ,  int c){
+	// Write your code here
+    sort(arr.begin(),arr.end());
+    int low = 1;
+    int high = arr[n-1]-arr[0];
+    int res = -1;
+    while(low<=high)
+    {
+        int mid = (low+high)>>1;
+        if(isPossible(arr,n,c,mid))
+        {
+            res  = mid;
+            low = mid+1;
+        }
+        else
+        {
+            high = mid-1;
+        }
+    }
+    return res;
+}
