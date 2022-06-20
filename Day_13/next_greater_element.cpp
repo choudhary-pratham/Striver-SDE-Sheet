1fe984fce6cf2daf1c1d616921d76f0c83464ed2
Pratham Choudhary <prathamchoudhary23@gmail.com>
2022-06-20 11:00:45 +0530

    added day 13 programs

---
+++ Day_13/next_greater_element.cpp
@@ -0,0 +1,16 @@
+#include<stack>
+vector<int> nextGreater(vector<int> &arr, int n) {
+    // Write your code here
+    stack<int>st;
+    vector<int>nge(n,-1);
+    for(int i=n-1;i>=0;i--)
+    {
+        while(!st.empty() && st.top()<=arr[i])
+        {
+            st.pop();
+        }
+        if(!st.empty()) nge[i] = st.top();
+        st.push(arr[i]);
+    }
+    return nge;
+}
