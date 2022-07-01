e49e86906ea9cb7278ca5af184273e6c36baa523
Pratham Choudhary <prathamchoudhary23@gmail.com>
2022-07-01 11:45:12 +0530

    Added program to find the kth largest element in a BST

---
+++ Day_21/kth_largest_element.cpp
@@ -0,0 +1,44 @@
+/************************************************************
+    Following is the Binary Search Tree node structure
+    
+    template <typename T>
+    class TreeNode {
+        public :
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~TreeNode() {
+            if(left)
+                delete left;
+            if(right)
+                delete right;
+        }
+    };
+
+************************************************************/
+void helper(TreeNode<int>* root,int &k,int &ans)
+{
+    if(root == NULL) return;
+    helper(root->right,k,ans);
+    k--;
+    if(k==0)
+    {
+        ans = root->data;
+        return;
+    }
+    helper(root->left,k,ans);
+}
+int KthLargestNumber(TreeNode<int>* root, int k) 
+{
+    // Write your code here.
+    int ans = -1;
+    helper(root,k,ans);
+    return ans;
+}
