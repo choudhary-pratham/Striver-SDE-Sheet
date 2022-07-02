7d870a14f0862e36f694cf0380ce3f832886420d
Pratham Choudhary <prathamchoudhary23@gmail.com>
2022-07-02 11:47:41 +0530

    Added program to convert a BT to Doubly LL

---
+++ Day_22/convert_a_binary_tree_to_doubly_LL.cpp
@@ -0,0 +1,38 @@
+/*************************************************************
+ 
+    Following is the Binary Tree node structure
+
+    class BinaryTreeNode 
+    {
+    public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
+void helper(BinaryTreeNode<int>* root,BinaryTreeNode<int>* &head,BinaryTreeNode<int>* &prev)
+{
+    if(root == NULL) return;
+    helper(root->left,head,prev);
+    if(prev == NULL) head = root;
+    else
+    {
+        prev->right = root;
+        root->left = prev;
+    }
+    prev = root;
+    helper(root->right,head,prev);
+}
+BinaryTreeNode<int>* BTtoDLL(BinaryTreeNode<int>* root) {
+    // Write your code here
+    BinaryTreeNode<int>* head = NULL,*prev = NULL;
+    helper(root,head,prev);
+    return head;
+}
