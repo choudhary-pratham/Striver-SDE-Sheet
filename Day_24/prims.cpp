cf69493f0f81739308c1338f3e34cd938c9ee225
Pratham Choudhary <prathamchoudhary23@gmail.com>
2022-07-07 16:33:19 +0530

    Added program to find mst using prims algo

---
+++ Day_24/prims.cpp
@@ -0,0 +1,37 @@
+#include<limits.h>
+#include<queue>
+vector<pair<pair<int, int>, int>> calculatePrimsMST(int n, int m, vector<pair<pair<int, int>, int>> &g)
+{
+    // Write your code here.
+    vector<pair<int,int>>adj[n+1];
+    for(int i=0;i<m;i++)
+    {
+        adj[g[i].first.first].push_back({g[i].first.second,g[i].second});
+        adj[g[i].first.second].push_back({g[i].first.first,g[i].second});
+    }
+    vector<int>key(n+1,INT_MAX),mst(n+1,0),parent(n+1,-1);
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+    pq.push({0,1});
+    key[1] = 0;
+    while(!pq.empty())
+    {
+        int node = pq.top().second;
+        pq.pop();
+        mst[node] = 1;
+        for(auto it:adj[node])
+        {
+            if(mst[it.first] == 0 && key[it.first] > it.second)
+            {
+                key[it.first] = it.second;
+                pq.push({key[it.first],it.first});
+                parent[it.first] = node;
+            }
+        }
+    }
+    vector<pair<pair<int, int>, int>> ans;
+    for(int i=2;i<=n;i++)
+    {
+        ans.push_back({{parent[i],i},key[i]});
+    }
+    return ans;
+}
